2b81874b95e44743c9f50a25317f548b3dfef8d3
AdskiyPonchik <tsoy507@gmail.com>
2025-04-16 09:06:27 +0200

    1 Tutorium

---
+++ src/aud/TPalindrome.java
@@ -0,0 +1,17 @@
+package aud;
+
+
+public class TPalindrome {
+  // Test if text is a T-palindrome.
+  // The input is guaranteed to include only valid characters, i.e.,
+  // those in {'a',...,'z','(',')','*'}. You don't need to check this.
+  //
+  public static boolean isTPalindrome(String text) {
+    // TODO: implementation
+    return false;
+  }
+
+  public static void main(String[] args) {
+    // TODO: test
+  }
+}
